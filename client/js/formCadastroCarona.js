39f416f1f95fc94688bc37d4d9ff570ca5165a12
Leonardo <leu1607@hotmail.com>
2017-06-22 21:31:35 -0300

    Adicionando Botão de interesse e campo quantidade de passageiros no formulario de cadastro de carona

--- client/js/formCadastroCarona.js
+++ client/js/formCadastroCarona.js
@@ -6,11 +6,15 @@ Template.formCadastroCarona.events({
 		var origemCarona = event.target.origemCarona.value;
 		var destinoCarona = event.target.destinoCarona.value;
 		var dataHoraCarona = event.target.dataHoraCarona.value;
+		var quantPassageiros = event.target.quantPassageiros.value;
 		if (isNotEmpty(origemCarona) && isNotEmpty(destinoCarona) && isNotEmpty(dataHoraCarona)) {
-			Meteor.call('addCarona', origemCarona, destinoCarona, dataHoraCarona);
+            /*if (quantPassageiros == null)//caso venha vazio, mas não sei fazer. Sou um coitado!
+                quantPassageiros = 0;*/
+			Meteor.call('addCarona', origemCarona, destinoCarona, dataHoraCarona, quantPassageiros);
 			event.target.origemCarona.value = "";
 			event.target.destinoCarona.value = "";
 			event.target.dataHoraCarona.value = "";
+            event.target.quantPassageiros.value = "";
 			Bert.alert("Carona cadastrada com sucesso!", "success", "growl-top-right");
 		} else {	
 			Bert.alert("Preencha todos os campos", "danger", "growl-top-right");
